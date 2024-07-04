64dc4d4994ef49b3b416969cc1c677c9cf326506
danimisme <msubhanr53@gmail.com>
2024-07-04 12:42:26 +0700

    create simple navbar

---
+++ src/components/Fragments/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="text-center p-6">
+        <h1 className="text-3xl font-semibold">TSM Pustaka</h1>
+      </div>
+    </nav>
+  );
+}
